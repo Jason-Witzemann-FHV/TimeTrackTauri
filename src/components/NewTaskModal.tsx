709c7f5c0f71d552fe4feaf00b2ae1d5c793c1af
Benjamin Mayer <lolgame99@gmail.com>
2023-12-07 18:16:13 +0100

    Changed inputs to datetime-local and added color input

--- src/components/NewTaskModal.tsx
+++ src/components/NewTaskModal.tsx
@@ -1,7 +1,13 @@
 import { createStore } from "solid-js/store";
+import { createSignal, For } from "solid-js";
 
 function NewTaskModal() {
     const [fields, setFields] = createStore();
+
+    const [presets, setPresets] = createSignal([
+        { name: "Preset 1", color: "#5534eb" },
+        { name: "Preset 2", color: "#3fed13" },
+    ]);
 
     const submitForm = () => {
         // form.submit()
@@ -10,67 +16,86 @@ function NewTaskModal() {
 
     return (
         <dialog id="newTaskModal" class="modal">
-            <form onSubmit={submitForm}>
-                <div class="modal-box">
-                    <h3 class="font-bold text-lg">Create new Task</h3>
+            <form onSubmit={submitForm} class="modal-box">
+                <h3 class="font-bold text-lg">Create new Task</h3>
+                <div class="flex flex-row">
                     <label class="form-control">
                         <div class="label">
                             <span class="label-text">Task Name</span>
                         </div>
                         <input
                             type="text"
-                            class="input input-bordered"
+                            class="input input-bordered w-11/12"
                             onInput={(e) => setFields("name", e.target.value)}
                         />
                     </label>
-                    <div class="flex flex-row mt-3">
-                        <div class="badge badge-primary">Preset 1</div>
-                        <div class="badge badge-primary">Preset 2</div>
-                    </div>
-                    <div class="flex flex-row">
-                        <label class="form-control">
-                            <div class="label">
-                                <span class="label-text">From</span>
+                    <label class="form-control w-1/2">
+                        <div class="label">
+                            <span class="label-text">Farbe</span>
+                        </div>
+                        <input
+                            type="color"
+                            class="input input-bordered h-full"
+                            onInput={(e) => setFields("color", e.target.value)}
+                        />
+                    </label>
+                </div>
+
+                <div class="flex flex-row mt-3">
+                    <For each={presets()}>
+                        {(preset, i) => (
+                            <div
+                                class="badge me-2"
+                                style={"background-color: " + preset.color}
+                            >
+                                {preset.name}
                             </div>
-                            <input
-                                type="text"
-                                class="input input-bordered w-2/3"
-                                onInput={(e) =>
-                                    setFields("startDate", e.target.value)
-                                }
-                            />
-                        </label>
-                        <label class="form-control">
-                            <div class="label">
-                                <span class="label-text">Until</span>
-                            </div>
-                            <input
-                                type="text"
-                                class="input input-bordered w-2/3"
-                                onInput={(e) =>
-                                    setFields("endDate", e.target.value)
-                                }
-                            />
-                        </label>
-                    </div>
+                        )}
+                    </For>
+                </div>
+                <div class="flex flex-row">
                     <label class="form-control">
                         <div class="label">
-                            <span class="label-text">Description</span>
+                            <span class="label-text">From</span>
                         </div>
-                        <textarea
-                            class="textarea textarea-bordered h-24"
+                        <input
+                            type="datetime-local"
+                            class="input input-bordered w-11/12"
                             onInput={(e) =>
-                                setFields("description", e.target.value)
+                                setFields("startDate", e.target.value)
                             }
-                        ></textarea>
+                        />
                     </label>
-
-                    <div class="modal-action">
-                        <button class="btn">Close</button>
-                        <button type="submit" class="btn">
-                            Create
-                        </button>
+                    <label class="form-control">
+                        <div class="label">
+                            <span class="label-text">Until</span>
+                        </div>
+                        <input
+                            type="datetime-local"
+                            class="input input-bordered w-11/12"
+                            onInput={(e) =>
+                                setFields("endDate", e.target.value)
+                            }
+                        />
+                    </label>
+                </div>
+                <label class="form-control">
+                    <div class="label">
+                        <span class="label-text">Description</span>
                     </div>
+                    <textarea
+                        class="textarea textarea-bordered h-24"
+                        onInput={(e) =>
+                            setFields("description", e.target.value)
+                        }
+                    ></textarea>
+                </label>
+
+                <div class="modal-action">
+                    <button class="btn">Close</button>
+                    <button type="submit" class="btn">
+                        Create
+                    </button>
                 </div>
             </form>
         </dialog>
