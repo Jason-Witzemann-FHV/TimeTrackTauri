c8ac215b0c49392a2c420f4f332f13b915fa8c80
Jason Witzemann <jason.witzemann@students.fhv.at>
2023-12-14 11:30:07 +0100

    DB init and types

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,39 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use serde::{Deserialize, Serialize};
+use ts_rs::TS;
+
+mod db;
+
+#[derive(Serialize, Deserialize, Debug, TS, Clone)]
+#[ts(export, export_to = "../src/types/")]
+struct Preset {
+    name: String,
+    color: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, TS, Clone)]
+#[ts(export, export_to = "../src/types/")]
+struct State {
+    task_id: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug, TS, Clone)]
+#[ts(export, export_to = "../src/types/")]
+struct Task {
+    id: i32,
+    name: String,
+    color: String,
+    start_date: i32,
+    end_date: i32,
+    description: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, TS, Clone)]
+#[ts(export, export_to = "../src/types/")]
+struct TaskModal {
+    task_id: i32,
+}
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -9,6 +43,11 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|_app| {
+            // Initialize the database.
+            db::init();
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
